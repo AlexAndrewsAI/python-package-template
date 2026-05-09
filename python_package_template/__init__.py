818d13dfe9b4aa1cafa13bc97444578d1f6ef4ec
AlexAndrewsAI <alex.andrews.ai@protonmail.com>
2026-05-09 14:45:56 -0400

    linting

--- python_package_template/__init__.py
+++ python_package_template/__init__.py
@@ -2,4 +2,4 @@ from python_package_template.config import Config
 from python_package_template.hello import HelloWorld
 
 __version__ = "0.1.0"
-__all__ = ["HelloWorld", "Config"]
+__all__ = ["Config", "HelloWorld"]
