e08cb3b3e108fae461f8b655f3b63ef894f1b5a0
AlexAndrewsAI <alex.andrews.ai@protonmail.com>
2026-07-11 23:16:23 +0000

    markdownlint

--- python_package_template/__init__.py
+++ python_package_template/__init__.py
@@ -4,8 +4,10 @@ A simple template for creating Python packages with configuration management
 and a hello world example.
 """
 
+from importlib.metadata import version
+
 from python_package_template.config import DEFAULT_CONFIG, Config
 from python_package_template.hello import HelloWorld
 
-__version__ = "0.1.1"
+__version__ = version("python-package-template")
 __all__ = ["DEFAULT_CONFIG", "Config", "HelloWorld", "__version__"]
